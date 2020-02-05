96884b45299379759b35c3965f4815721e3af43c
ramprasathhcl1985 <ramprasath.ganapathy@hcl.com>
2020-02-05 20:15:48 +0530

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { LoginComponent } from './auth/components/login/login.component';
+import { AdminComponent } from './auth/components/admin/admin.component';
+import { VendorComponent } from './auth/components/vendor/vendor.component';
+import { AddvendorComponent } from './auth/components/addvendor/addvendor.component'
+
 
 const routes: Routes = [
   { path: '', component: LoginComponent },
-  { path: 'login', component: LoginComponent },
+  { path: 'admin', component: AdminComponent },
+  { path: 'vendoritem', component: VendorComponent },
+  { path: 'addvendor', component: AddvendorComponent },
   { path: 'items', loadChildren: () => import('./items/items.module').then(m => m.ItemsModule) },
   { path: 'vendors', loadChildren: () => import('./vendors/vendors.module').then(m => m.VendorsModule) },
   { path: 'orders', loadChildren: () => import('./orders/orders.module').then(m => m.OrdersModule) }
--- src/app/auth/components/vendor/vendor.component.ts
+++ src/app/auth/components/vendor/vendor.component.ts
@@ -10,12 +10,49 @@ import { Router, ActivatedRoute } from '@angular/router';
 })
 export class VendorComponent implements OnInit {
 
+  additems: FormGroup;
+  submitted = false;
+
   constructor(private httpService: HttpService, private formBuilder: FormBuilder, private router: Router) { }
 
   ngOnInit() {
+
+
+
+    this.additems = this.formBuilder.group({
+
+      itemName: ['', Validators.required],
+      itemType: ['Select Role', Validators.required],
+      price: ['', Validators.required],
+      userid: ['', Validators.required],
+      quantity: ['', Validators.required],
+
+
+
+
+    });
+
+
   }
 
+  get f() { return this.additems.controls; }
+  onSubmit() {
 
+
+    this.submitted = true;
+
+    console.log(this.additems.value);
+    this.httpService.postRequest('/item/addItem', this.additems.value).subscribe(
+      (response) => console.log(response),
+      (error) => console.log(error)
+    )
+
+    // stop here if form is invalid
+    if (this.additems.invalid) {
+      return;
+    }
+
+  }
 
 }
 

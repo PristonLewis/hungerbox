b8e757eee5839562407394e82db4e950c5fd38d1
ramprasathhcl1985 <ramprasath.ganapathy@hcl.com>
2020-02-05 16:55:57 +0530

    admin page

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -4,12 +4,14 @@ import { LoginComponent } from './components/login/login.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
+import { AdminComponent } from './components/admin/admin.component';
+import { VendorComponent } from './components/vendor/vendor.component';
 
 
 
 
 @NgModule({
-    declarations: [LoginComponent],
+    declarations: [LoginComponent, AdminComponent, VendorComponent],
     imports: [
         CommonModule,
         BrowserAnimationsModule,
--- src/app/auth/components/login/login.component.ts
+++ src/app/auth/components/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpService } from 'src/app/shared/http.service';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router, ActivatedRoute } from '@angular/router';
 
 
 @Component({
@@ -12,11 +13,11 @@ export class LoginComponent implements OnInit {
   registerForm: FormGroup;
   submitted = false;
 
-  Roledetails: any = ['admin', 'Vendor', 'user'];
+  Roledetails: any = ['admin', 'vendor', 'user'];
 
 
 
-  constructor(private httpService: HttpService, private formBuilder: FormBuilder) {
+  constructor(private httpService: HttpService, private formBuilder: FormBuilder, private router: Router) {
 
   }
 
@@ -37,14 +38,30 @@ export class LoginComponent implements OnInit {
 
     this.submitted = true;
 
+
     this.httpService.postRequest('users/login', this.registerForm.value).subscribe(
       (response) => console.log(response),
       (error) => console.log(error)
     )
 
+
+    if (this.registerForm.value.roleName == 'admin') {
+
+      this.router.navigate(['/admin']);
+
+    }
+    if (this.registerForm.value.roleName == 'vendor') {
+
+      this.router.navigate(['/orders']);
+
+    }
+    if (this.registerForm.value.roleName == 'user') {
+
+      this.router.navigate(['/vendors']);
+
+    }
+
     console.log(this.registerForm.value);
-
-
 
 
     // stop here if form is invalid
